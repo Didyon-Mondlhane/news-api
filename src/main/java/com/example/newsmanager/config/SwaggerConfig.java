d941fbf3e9ae4b3f0847aca6a92981d1960b910f
Didyon-Mondlhane <mondlhanedj5@gmail.com>
2025-06-07 00:10:47 +0200

    chore: configura swagger para login

--- src/main/java/com/example/newsmanager/config/SwaggerConfig.java
+++ src/main/java/com/example/newsmanager/config/SwaggerConfig.java
@@ -1,5 +1,29 @@
 package com.example.newsmanager.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
-    
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("News Manager API")
+                        .version("1.0")
+                        .description("API para gestão de notícias"))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .name("JWT")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
 }
