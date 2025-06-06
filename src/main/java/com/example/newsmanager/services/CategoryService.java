5fa7d1f960c8cdba831b00733f0f5801e1db889c
Arlindo Lazaro <arlindolazaro202@gmail.com>
2025-06-06 11:45:03 +0200

    feat(service): cria estrutura básica do CategoryService

--- src/main/java/com/example/newsmanager/services/CategoryService.java
+++ src/main/java/com/example/newsmanager/services/CategoryService.java
@@ -1,5 +1,17 @@
 package com.example.newsmanager.services;
 
+import com.example.newsmanager.domain.category.*;
+import com.example.newsmanager.repositories.CategoryRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
-    
+    private final CategoryRepository categoryRepository;
+
+    // Métodos CRUD serão implementados aqui
 }
