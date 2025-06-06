d31cc82c469b3766f30c3099d5eb2ad5b5535633
Didyon-Mondlhane <mondlhanedj5@gmail.com>
2025-06-06 11:54:10 +0200

    feat: adiciona métodos de verificação de estado da conta

--- src/main/java/com/example/newsmanager/security/UserDetailsImpl.java
+++ src/main/java/com/example/newsmanager/security/UserDetailsImpl.java
@@ -22,4 +22,33 @@ public class UserDetailsImpl implements UserDetails {
         return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
