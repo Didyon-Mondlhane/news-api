3c018662363b8d5e20f2e54d73a06e91cb5aef07
Didyon-Mondlhane <mondlhanedj5@gmail.com>
2025-06-06 11:53:55 +0200

    feat: implementa UserDetails para integração com Spring Security

---
+++ src/main/java/com/example/newsmanager/security/UserDetailsImpl.java
@@ -0,0 +1,25 @@
+package com.example.newsmanager.security;
+
+import com.example.newsmanager.domain.auth.User;
+import com.example.newsmanager.domain.auth.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRole role = user.getRole();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+}
