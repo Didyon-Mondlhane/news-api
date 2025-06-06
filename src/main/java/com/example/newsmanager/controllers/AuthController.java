0a60f552b3a12f23ba0389727ce9ed59a47fed11
Didyon-Mondlhane <mondlhanedj5@gmail.com>
2025-06-06 11:44:51 +0200

    feat: adiciona método de login no AuthController

--- src/main/java/com/example/newsmanager/controllers/AuthController.java
+++ src/main/java/com/example/newsmanager/controllers/AuthController.java
@@ -1,5 +1,37 @@
 package com.example.newsmanager.controllers;
 
+import com.example.newsmanager.domain.auth.*;
+import com.example.newsmanager.security.JwtService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
 public class AuthController {
-    
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(
+                loginRequest.email(), 
+                loginRequest.password());
+        
+        var auth = authenticationManager.authenticate(usernamePassword);
+        var user = (User) auth.getPrincipal();
+        var token = jwtService.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
+
+
 }
