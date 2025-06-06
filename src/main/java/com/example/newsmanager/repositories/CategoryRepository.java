294607de202e98c4d3575a7ce7cdc65a2311e52e
Arlindo Lazaro <arlindolazaro202@gmail.com>
2025-06-06 11:18:52 +0200

    feat: implementa repositório para Category com métodos customizados

--- src/main/java/com/example/newsmanager/repositories/CategoryRepository.java
+++ src/main/java/com/example/newsmanager/repositories/CategoryRepository.java
@@ -1,5 +1,10 @@
 package com.example.newsmanager.repositories;
 
-public class CategoryRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.newsmanager.domain.category.Category;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+    boolean existsByName(String name);
 }
