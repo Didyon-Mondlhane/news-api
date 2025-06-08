b3a62dec0be183b44899c122525f43602f87ced2
Didyon-Mondlhane <mondlhanedj5@gmail.com>
2025-06-08 13:24:07 +0200

    feat: adiciona repositório para gestão de reações

--- src/main/java/com/example/newsmanager/repositories/ReactionRepository.java
+++ src/main/java/com/example/newsmanager/repositories/ReactionRepository.java
@@ -1,5 +1,11 @@
 package com.example.newsmanager.repositories;
 
-public class ReactionRepository {
-    
+import com.example.newsmanager.domain.reaction.Reaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReactionRepository extends JpaRepository<Reaction, Long> {
+    List<Reaction> findByNewsId(String newsId);
+    List<Reaction> findByUserId(Long userId);
+    boolean existsByUserIdAndNewsId(Long userId, String newsId);
 }
